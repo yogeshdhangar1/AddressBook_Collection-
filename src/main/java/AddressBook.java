be5672ea7df973622b5163f2229e423096b69ea8
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-10-05 04:18:34 -0700

    Added: Create Contact

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBook {
+    static List<Person> personList = new ArrayList<Person>(); 
     public static void main(String[] args) {
         System.out.println("Welcome To AddressBook Program");
+        CreateContact();
+        display();
+        
+    }
+
+    private static void display() {
+        System.out.println(personList);
+    }
+
+    private static void CreateContact() {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter The Name");
+        String Name = sc.nextLine();
+        System.out.println("Enter the Address");
+        String Address = sc.nextLine();
+        System.out.println("Enter The City ");
+        String City = sc.nextLine();
+        System.out.println("Enter the Email ");
+        String Email = sc.nextLine();
+        System.out.println("Enter the Number");
+        int Number = sc.nextInt();
+        System.out.println("Enter The Zip Code ");
+        int Zip = sc.nextInt();
+      Person pr = new Person(Name,Email,City,Email,Zip,Number);
+      personList.add(pr);
     }
 }
---
+++ src/main/java/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    String Name ;
+    String Address;
+    String City;
+    String Email;
+    int zip;
+    int Number ;
+
+    public Person(String name, String address, String city, String email, int zip, int number) {
+        Name = name;
+        Address = address;
+        City = city;
+        Email = email;
+        this.zip = zip;
+        Number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Name='" + Name + '\'' +
+                ", Address='" + Address + '\'' +
+                ", City='" + City + '\'' +
+                ", Email='" + Email + '\'' +
+                ", zip=" + zip +
+                ", Number=" + Number +
+                '}';
+    }
+}
