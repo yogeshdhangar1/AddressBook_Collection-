830bc1370d08d11f3be3dfb601b3aa4f0be23fcd
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-11-09 23:24:48 -0800

    Added: Read And write csv File

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,15 +1,25 @@
+import com.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.List;
 
 public class AddressBook {
-    public static void main(String[] args) {
-        Person.uniqueData();
-        System.out.println("Search Result to show multiple person in the city or state");
-     Person.searchPersonInCity("Pune");
-     Person.sortAphabeticallyByPersonName();
+    public void WriteData() throws IOException {
+        FileWriter fileWriter = new FileWriter("C:\\intallije workplace\\AddressBookCollection\\AddressBook.csv.txt");
+        CSVWriter csvWriter = new CSVWriter(fileWriter);
+        List<String[]> data = new ArrayList<>();
+        data.add(new String[]{"Name","Address","City","Email","Number"});
+        data.add(new String[]{"Yogesh","Wagholi","pune","Abc@123","773636"});
+        data.add(new String[]{"Bhavesh","nizar","Nandurbar","Xyz@123","376353"});
+        data.add(new String[]{"Ketan","Thane","Mumbai","Ketan@123","6643"});
+        csvWriter.writeAll(data);
+        csvWriter.close();
+    }
+    public static void main(String[] args) throws IOException {
+        AddressBook obj = new AddressBook();
+        obj.WriteData();
+
     }
 }
-
-
-
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,12 +1,6 @@
-import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Set;
-
 public class Person {
-    static HashMap<String, String> map = new HashMap<String, String>();
-    static Scanner sc = new Scanner(System.in);
-
-    public static void uniqueData() {
+    public  static void  uniqueData() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter The Name");
         String name = sc.next();
@@ -16,12 +10,9 @@ public class Person {
         String Address = sc.next();
         System.out.println("Enter The Mail");
         String email = sc.next();
-        System.out.println("Enter The City");
-        String city = sc.next();
 
     }
-
-    public static String newAddressBookEnter() {
+    public static String newAddressBookEnter(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter The Name");
         String name = sc.next();
@@ -33,46 +24,4 @@ public class Person {
         String email = sc.next();
         return name;
     }
-
-    public static void searchPersonInCity(String city) {
-        //  initiating the value and "count" value use for the NO PERSON PRESENT IN CITY
-        String str;
-        String[] arrOfStr;
-        int count = 0;
-        //  for loop for HASHMAP
-        Set<String> keys2 = map.keySet();
-        for (Object key : keys2) {
-            str = map.get(key);
-            arrOfStr = str.split(" ", 4);
-            for (String temp : arrOfStr) {
-                //System.out.println(a);
-                if (temp.equals(city)) {
-                    System.out.println("Keys :  " + key + " value:  " + str);
-                    //  Split The HashMAP Value Pair
-                    arrOfStr = str.split(" ", 4);
-                    for (String t : arrOfStr) {
-                        // splittd Value will be compare to the City
-                        if (temp.equals(city)) {
-                            System.out.println("Keys :  " + key + " value:  " + str);
-                            count++;
-                        }
-                    }
-                }
-            }
-            // count will null it means No One Available in City
-            if (count == 0)
-                System.out.println("No Deails available");
-        }
-        System.out.println(count+"  number of person stay in the "+city);
-        // count will null it means No One Available in City
-        if (count==1)
-            System.out.println("one contact available");
-
-    }
-    public static void sortAphabeticallyByPersonName() {
-        map.keySet().stream().sorted((a1, a2) -> a2.compareTo(a1)).forEach(x -> System.out.println(x));
-
-    }
-
 }
-
